e99e4063b72c43d7d8b3bec9da2dd295882b4315
Morgan-Ngetich <ngetichmorgan6@gmail.com>
2023-10-02 03:12:10 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,49 @@
-// Write your solution in this file!
+//Original employee object
+const employee = {
+  name: "Sam",
+  streetAddress: "11 Broadway",
+}
+
+//Function to update an employee,s property non-destructively
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+  const updatedEmployee = { ...employee}
+  updatedEmployee[key] = value
+  return updatedEmployee
+}
+
+//Function to update an emlpoyee's property destructively
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+  employee[key] = value
+  return employee
+}
+
+//Function to delete an employee's property non-destructively
+function deleteFromEmployeeByKey(employee, key) {
+const newEmployee = { ...employee }
+delete newEmployee[key]
+return newEmployee
+}
+
+//Function to delete an emloyee's property destructively
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+  delete employee[key]
+    return employee
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
+ 
+
